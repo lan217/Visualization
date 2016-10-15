2c80901b64f254f0c41024b8b4a3c7a30ee0036b
lan217 <zcj19930517@163.com>
2016-10-15 13:34:15 +0800

    材料库db,增删

--- material_lib.h
+++ material_lib.h
@@ -4,6 +4,7 @@
 #include <QStringList>
 #include <QList>
 #include <QMap>
+#include <QHash>
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
@@ -12,14 +13,16 @@ class MaterialLib
 {
 
 public:
-    MaterialLib();
-    ~MaterialLib();
+
     class Material;
     static MaterialLib *getInstance();
-    QMap<int, Material> getMaterialNameList();
+    QList<Material> getMaterialNameList();
     QMap<float, float> getTmpLambdaMap(int index);
     QMap<float, float> getTmpCpMap(int index);
     Material* getMaterial(int index);
+    bool addMaterial(const QString& name, int tyle);
+    bool deleteMaterial(int index);
+    bool isExist(const QString& name);
     bool updateMaterialName(int index, const QString &name);
     bool updateMaterialProperty(int index, float destiny ,float emissivity);
     bool updateMaterialTmpLambdaMap(int index, QMap<float, float> tmpLambdaMap);
@@ -27,6 +30,7 @@ public:
 
     class Material {
     public:
+        int id;
         QString materialName;
         float destiny;
         float emissivity;
@@ -35,6 +39,8 @@ public:
 
 
 private:
+    MaterialLib();
+    ~MaterialLib();
     const static QString CREATE_MATERIAL_TABLE_SQL;
     const static QString CREATE_TMPLAMBDA_TABLE_SQL;
     const static QString CREATE_TMPCP_TABLE_SQL;
--- my_treewidget_item.h
+++ my_treewidget_item.h
@@ -11,7 +11,7 @@ class MyQtreeWidgetItem : public QObject ,public QTreeWidgetItem
     Q_OBJECT
 public:
     explicit MyQtreeWidgetItem(QTreeWidgetItem *parent = 0, int index = -1);
-    explicit MyQtreeWidgetItem(MyQtreeWidgetItem *parent = 0, int index = -1);
+    explicit MyQtreeWidgetItem(MyQtreeWidgetItem *parent, int index = -1);
     ~MyQtreeWidgetItem();
     void init();
     void setIndex(int index);
